a65ae4d15ed751415100beaddb6aa81dcef996dc
josemvegar <josevega1999.16@gmail.com>
2019-11-02 21:50:33 -0400

    listo

--- Jose_27000723.py
+++ Jose_27000723.py
@@ -8,8 +8,7 @@ Cada participante debe completar su módulo y luego solicitar el Pull-Request.
 import math
 
 
-def biseccion(a, b, ER, N, mx, m):
-    err= float
+def biseccion(a, b, ER, N, mx, m, n,err):
     """
     Implementa el Algoritmo de Biseccion y retorna la aproximación de la raiz.
 
@@ -35,33 +34,65 @@ def biseccion(a, b, ER, N, mx, m):
         err= (mx - m) / mx
         if (err <0):
             err=err*-1
-        print("Iteración:", i, "Punto Medio:", mx, "Error:", err)
-        biseccion (a, b , ER, N, mx, m)
+        if (err> ER):
+            if (n!=N):
+                print("Iteración:", n, "Punto Medio:", mx, "Error:", err)
+                mx=biseccion (a, b , ER, N, mx, m, n, err)
+            else:
+                print("Iteración:", n, "Punto Medio:", mx, "Error:", err)
+                print ("Fin de las iteraciones por cantidad máxima alcanzada.")
+                return mx
+        else:
+            print("Iteración:", n, "Punto Medio:", mx, "Error:", err)
+            print ("Fin de las iteraciones,  error menor el indicado.")
+            return mx
     else:
         n=n+1
         m=(a+b)/2
         mx=m
-        print("Iteración:", N, "Punto Medio:", mx, "Error: La primera iteracioón no lleva cálculo de error")
-        biseccion(a,b,ER,N,mx, m)
+        print("Iteración:", n, "Punto Medio:", mx, "Error: La primera iteración no lleva cálculo de error")
+        mx= biseccion(a,b,ER,N,mx, m, n, err)
     return mx
 
-def funcion (a):
-    return a+a
+def funcion (x):
+    return math.cos(x)- (x)
 
 if __name__ == "__main__":
     # Pruebe aquí su función.
     b=float
     a=float    
     err= float
-    print ("Algoritmo de bisección para aproximar las raices de una función.")
-    print ("Ingrese los valores para los extremos A y B")
-    print ("Ingrese valor de A: ")
-    a= input ()
-    print ("Igrese el valor de B")
-    b= input ()
-    print ("Ingrese el valor del error para d1etener el algoritmo")
-    err= input ()
-    print ("¿Cuántas veces se va a iterar si no ha llegado al error?")
-    n= input()
-    print (biseccion(a, b, err, n,0,0))
+    rep= "s"
+    while (rep =="s" or rep =="S"):
+        print ("Algoritmo de bisección para aproximar las raices de una función.")
+        print ("Ingrese los valores para los extremos A y B")
+        print ("Ingrese valor de A: ")
+        a= float (input ())
+        print ("Igrese el valor de B")
+        b= float (input ())
+        while (funcion(a)*funcion(b) > 0):
+            print ("Estos valores no están dentro de un cambio de signo, ingrese otros valores correctos:")
+            print ("Valor de a")
+            a= float (input ())
+            print ("Valor de b")
+            b= float ( input ())
+        print ("Ingrese el valor del error para d1etener el algoritmo")
+        err=float (input ())
+        print (err)
+        while (err<0):
+            print ("El error no puede ser menor a 0, ingrese un valor correcto.")
+            err= float (input ())
+        print ("¿Cuántas veces se va a iterar si no ha llegado al error?")
+        n= float (input ())
+        while (n< 1):
+            print ("La cantidad de iteraciones no puede ser menor a 1, ingrese un valor correcto")
+            n= input ()
+        print ("el punto medio más cercano a la raiz es: ",biseccion(a, b, err, n,0,0,0,0))
+        print ("fin del algoritmo, desea repetir las operacones (n/s) ?")
+        rep= input ()
+        while (rep != "n" and rep != "s"):
+            print ("Ingrese un valor correcto (n/s)")
+            rep= input()
+    print ("Bye.")
+    chao=input()
     pass
