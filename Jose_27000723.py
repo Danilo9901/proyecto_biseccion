595cf565b42dc5dff77bdc1f4ac874c48c941665
josemvegar <josevega1999.16@gmail.com>
2019-11-02 23:51:23 -0400

    listov2

--- Jose_27000723.py
+++ Jose_27000723.py
@@ -8,7 +8,11 @@ Cada participante debe completar su módulo y luego solicitar el Pull-Request.
 import math
 
 
-def biseccion(a, b, ER, N, mx, m, n,err):
+#def biseccion(a, b, ER, N, mx, m, n,err):
+def biseccion(f, a, b, ER, N):
+    n=int
+    m=float
+    mx =float
     """
     Implementa el Algoritmo de Biseccion y retorna la aproximación de la raiz.
 
@@ -19,46 +23,55 @@ def biseccion(a, b, ER, N, mx, m, n,err):
     ER: cota mínima del error relativo.
     N: número máximo de iteraciones.
     """
-    if (n != 0):
-        if (funcion(a)>0 and funcion (mx)<0):
-            b= mx
-        if ( funcion (a)<0 and funcion (mx) >0):
-            a=a
-        if (funcion (mx)>0 and funcion (b)<0 ):
-            a= mx
-        if ( funcion (mx)<0 and funcion (b)>0):
-            b= b
-        m=mx
-        mx=(a+b)/2
-        n=n+1
-        err= (mx - m) / mx
-        if (err <0):
-            err=err*-1
-        if (err> ER):
-            if (n!=N):
-                print("Iteración:", n, "Punto Medio:", mx, "Error:", err)
-                mx=biseccion (a, b , ER, N, mx, m, n, err)
+    n=0
+    while (n<N):
+        if (n != 0):
+            if (f(a)>0 and f(mx)<0):
+                b= mx
+            if ( f(a)<0 and f(mx) >0):
+                a=a
+            if (f(mx)>0 and f(b)<0 ):
+                a= mx
+            if ( f(mx)<0 and f(b)>0):
+                b= b
+            mx=(a+b)/2
+            n=n+1
+            err= (mx - m) / mx
+            m=mx
+            if (err <0):
+                err=err*-1
+            if (err> ER):
+                if (n!=N):
+                    print("Iteración:", n, "Punto Medio:", mx, "Error:", err)
+                    #mx=biseccion (a, b , ER, N, mx, m, n, err)
+                else:
+                    print("Iteración:", n, "Punto Medio:", mx, "Error:", err)
+                    print ("Fin de las iteraciones por cantidad máxima alcanzada.")
+                    n=N
             else:
                 print("Iteración:", n, "Punto Medio:", mx, "Error:", err)
-                print ("Fin de las iteraciones por cantidad máxima alcanzada.")
-                return mx
+                print ("Fin de las iteraciones,  error menor el indicado.")
+                n=N
         else:
-            print("Iteración:", n, "Punto Medio:", mx, "Error:", err)
-            print ("Fin de las iteraciones,  error menor el indicado.")
-            return mx
-    else:
-        n=n+1
-        m=(a+b)/2
-        mx=m
-        print("Iteración:", n, "Punto Medio:", mx, "Error: La primera iteración no lleva cálculo de error")
-        mx= biseccion(a,b,ER,N,mx, m, n, err)
+            n=n+1
+            m=(a+b)/2
+            mx=m
+            print("Iteración:", n, "Punto Medio:", mx, "Error: La primera iteración no lleva cálculo de error")
+            #mx= biseccion(a,b,ER,N,mx, m, n, err)
+            
     return mx
 
-def funcion (x):
-    return math.cos(x)- (x)
+def f(x):
+    print ("f(x)")
+    return lambda x: math.cos(x)- (x)
+    #esta y la f de abajo se deben cambiar las 2
 
 if __name__ == "__main__":
     # Pruebe aquí su función.
+    #esta y la f de arriba se deben cambiar las 2
+
+    f=lambda x: math.cos(x)-x
+    
     b=float
     a=float    
     err= float
@@ -70,15 +83,18 @@ if __name__ == "__main__":
         a= float (input ())
         print ("Igrese el valor de B")
         b= float (input ())
-        while (funcion(a)*funcion(b) > 0):
+        fa=f(a)
+        fb=f(b)
+        while (fa*fb > 0):
             print ("Estos valores no están dentro de un cambio de signo, ingrese otros valores correctos:")
             print ("Valor de a")
             a= float (input ())
             print ("Valor de b")
-            b= float ( input ())
+            b= float (input ())
+            fa=f(a)
+            fb(b)
         print ("Ingrese el valor del error para d1etener el algoritmo")
         err=float (input ())
-        print (err)
         while (err<0):
             print ("El error no puede ser menor a 0, ingrese un valor correcto.")
             err= float (input ())
@@ -87,7 +103,8 @@ if __name__ == "__main__":
         while (n< 1):
             print ("La cantidad de iteraciones no puede ser menor a 1, ingrese un valor correcto")
             n= input ()
-        print ("el punto medio más cercano a la raiz es: ",biseccion(a, b, err, n,0,0,0,0))
+        #print ("el punto medio más cercano a la raiz es: ",biseccion(a, b, err, n,0,0,0,0))
+        print ("el punto medio más cercano a la raiz es: ",biseccion(f,a,b,err,n))
         print ("fin del algoritmo, desea repetir las operacones (n/s) ?")
         rep= input ()
         while (rep != "n" and rep != "s"):
